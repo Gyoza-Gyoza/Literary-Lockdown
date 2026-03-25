e72825e4df358782884f0fe54069e8b9203812f8
zekingfx <zekingfx@gmail.com>
2026-03-25 23:58:14 +0800

    Split Up location manager into better pieces. but the stick movement suddenly dont work

--- Assets/Scripts/UI/New UI/LobbyButtons.cs
+++ Assets/Scripts/UI/New UI/LobbyButtons.cs
@@ -5,8 +5,35 @@ using UnityEngine.UI;
 
 public class LobbyButtons : MonoBehaviour
 {
+    public Image hostButton;
+    public Image joinButton;
+
+    public Sprite hostUnavailSprite;
+    public Sprite hostAvailSprite;
+    public Sprite joinUnavailSprite;
+    public Sprite joinAvailSprite;
+
+    private bool locked = false;
+
+    private void Update()
+    {
+        if (LocationManager.Instance == null || !LocationManager.Instance.isLocationValid)
+        {
+            hostButton.sprite = hostUnavailSprite;
+            joinButton.sprite = joinUnavailSprite;
+            locked = true;
+        }
+        else
+        {
+            hostButton.sprite = hostAvailSprite;
+            joinButton.sprite = joinAvailSprite;
+            locked = false;
+        }
+    }
+
     public async void HostLobby()
     {
+        if (locked) return; 
         // Set raid details in lobby details script
         RaidSettings raidSettings = GameObject.FindFirstObjectByType<RaidSettings>();
         raidSettings.LockInSettings();
@@ -22,6 +49,8 @@ public class LobbyButtons : MonoBehaviour
 
     public async void JoinLobby()
     {
+        if (locked) return;
+
         await SceneManager.LoadSceneAsync("Game Lobby");
         
         GameObject.Find("Loading Screen").SetActive(false);
