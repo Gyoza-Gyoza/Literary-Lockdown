c061a9b7860565ab1663193360432968907573e4
zekingfx <zekingfx@gmail.com>
2026-03-26 17:21:16 +0800

    Updated Raid Menu

--- Assets/Scripts/UI/New UI/RaidSettings.cs
+++ Assets/Scripts/UI/New UI/RaidSettings.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RaidSettings : MonoBehaviour
 {
@@ -9,13 +10,26 @@ public class RaidSettings : MonoBehaviour
     public GameObject lobbyDetailsGO;
     private LobbyDetails lobbyDetails;
 
+
     [Header("Difficulty")]
     public TextMeshProUGUI diffcultyTxt;
+    public Button decreaseDiff_Button;
+    public Button increaseDiff_Button;
+    public Sprite decreaseDiffActive_Sprite;
+    public Sprite decreaseDiffInactive_Sprite;
+    public Sprite increaseDiffActive_Sprite;
+    public Sprite increaseDiffInactive_Sprite;
     private List<string> difficultyList = new List<string>{"EASY", "MEDIUM", "HARD"};
     private int difficultyIndex = 0;
 
     [Header("Timer")]
     public TextMeshProUGUI timerTxt;
+    public Button increaseTime_Button;
+    public Button decreaseTime_Button;
+    public Sprite decreaseTimeActive_Sprite;
+    public Sprite decreaseTimeInactive_Sprite;
+    public Sprite increaseTimeActive_Sprite;
+    public Sprite increaseTimeInactive_Sprite;
     private List<int> timerList = new List<int> {300, 600, 900, 1800, 2700, 3600};
     private int timerIndex = 0;
 
@@ -66,10 +80,20 @@ public class RaidSettings : MonoBehaviour
         if (difficultyIndex == difficultyList.Count - 1)
         {
             Debug.Log("Reached max difficulty!");
+            // Insert visual feedback here
+            
             return;
         }
 
+        decreaseDiff_Button.image.sprite = decreaseDiffActive_Sprite;
+
         difficultyIndex++;
+
+        if (difficultyIndex == difficultyList.Count - 1)
+        {
+            increaseDiff_Button.image.sprite = increaseDiffInactive_Sprite;
+        }
+
         UpdateDifficulty();
     }
 
@@ -78,10 +102,19 @@ public class RaidSettings : MonoBehaviour
         if (difficultyIndex == 0)
         {
             Debug.Log("Reached min difficulty!");
+            // Insert visual feedback here
             return;
         }
 
+
+        increaseDiff_Button.image.sprite = increaseDiffActive_Sprite;
         difficultyIndex--;
+
+        if (difficultyIndex == 0)
+        {
+            decreaseDiff_Button.image.sprite = decreaseDiffInactive_Sprite;
+        }
+
         UpdateDifficulty();
     }
 
@@ -103,7 +136,14 @@ public class RaidSettings : MonoBehaviour
             return;
         }
 
+        decreaseTime_Button.image.sprite = decreaseTimeActive_Sprite;
         timerIndex++;
+
+        if (timerIndex == timerList.Count - 1)
+        {
+            increaseTime_Button.image.sprite = increaseTimeInactive_Sprite;
+        }
+
         UpdateTimer();
     }
 
@@ -115,7 +155,14 @@ public class RaidSettings : MonoBehaviour
             return;
         }
 
+        increaseTime_Button.image.sprite = increaseTimeActive_Sprite;
         timerIndex--;
+
+        if (timerIndex == 0)
+        {
+            decreaseTime_Button.image.sprite = decreaseTimeInactive_Sprite;
+        }
+
         UpdateTimer();
     }
 
